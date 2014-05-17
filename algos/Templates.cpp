532bc811c547c6927a0a5a9ce07e907ac4aa6007
Ivan Jobs <ivan1377@163.com>
2014-05-17 19:05:14 +0800

    增加最大团模板

--- algos/Templates.cpp
+++ algos/Templates.cpp
@@ -61,6 +61,7 @@ void dfs(int cur, int father, int dep, int n) {
  | CALL: dfs(0, -1, 1, n);
  | k = deg[0], deg[i] + 1 (i = 1...n-1) 为删除该节点后得到的连通图个数。
  | 注意： 0 作为根比较特殊！
+ | 没搞明白这个函数的意思：（
 \*===================================================*/
 int edge[V][V], anc[V], pre[V], vis[V], deg[V];
 void dfs(int cur, int father, int dep, int n) {
@@ -82,5 +83,41 @@ void dfs(int cur, int father, int dep, int n) {
 		}
 	}
 	vis[cur] = 2;
+}
+
+/*=======================================================*\
+ | 最大团问题 DP + DFS
+ | INIT： g[][] 邻接矩阵
+ | CALL： res = clique(n);
+\*=======================================================*/
+
+int g[V][V], dp[V], stk[V][V], mx;
+int dfs(int n, int ns, int dep) {
+	if (0 == ns) {
+		if (dep > mx) mx = dep;
+		return 1;
+	}
+	int i, j, k, p, cnt;
+	for (i = 0; i < ns; ++i) {
+		k = stk[dep][i]; cnt = 0;
+		if (dep + n - k <= mx) return 0;
+		if (dep + dp[k] <= mx) return 0;
+		for (j = i + 1; j < ns; ++j) {
+			p = stk[dep][j];
+			if (g[k][p]) stk[dep + 1][cnt++] = p;
+		}
+		dfs(n, cnt, dep + 1);
+	}
+	return 1;
+}
+int clique(int n) {
+	int i, j, ns;
+	for (mx = 0, i = n - 1; i >= 0; i--) {
+		// vertex: 0 ~ n-1
+		for (ns = 0, j = i + 1; j < n; ++j)
+			if (g[i][j]) stk[1][ns++] = j;
+		dfs(n, ns, 1); dp[i] = mx;
+	}
+	return mx;
 }
 
