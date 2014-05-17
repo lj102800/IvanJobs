88e0b3abfdae81ccbb491f389f81574fd9d2a25d
Ivan Jobs <ivan1377@163.com>
2014-05-17 18:42:14 +0800

    add uG connect degree?

--- algos/Templates.cpp
+++ algos/Templates.cpp
@@ -54,3 +54,33 @@ void dfs(int cur, int father, int dep, int n) {
 	vis[cur] = 2;
 }
 
+
+/*==================================================*\
+ | 无向图连通度（割）
+ | INIT： edge[][]邻接矩阵；vis[], pre[], anc[], deg[] 置为0
+ | CALL: dfs(0, -1, 1, n);
+ | k = deg[0], deg[i] + 1 (i = 1...n-1) 为删除该节点后得到的连通图个数。
+ | 注意： 0 作为根比较特殊！
+\*===================================================*/
+int edge[V][V], anc[V], pre[V], vis[V], deg[V];
+void dfs(int cur, int father, int dep, int n) {
+	// vertex: 0 ~ n-1
+	int cnt = 0;
+	vis[cur] = 1; pre[cur] = anc[cur] = dep;
+	for (int i = 0; i < n; ++i) if (dege[cur][i]) {
+		if (i != father && 1 == vis[i]) {
+			if (pre[i] < anc[cur])
+				anc[cur] = pre[i]; //back edge
+		}
+		if (0 == vis[i]) { // tree edge
+			dfs(i, cur, dep + 1, n);
+			++cnt; // branches number
+			if (anc[i] < anc[cur]) anc[cur] = anc[i];
+			if ((curr == 0 && cnt > 1) ||
+				(cnt != 0 && anc[i] >= pre[cur]))
+					++deg[cur];
+		}
+	}
+	vis[cur] = 2;
+}
+
