e93d96b069bd4adece204210fea532c405c1ccbb
Ivan Jobs <ivan1377@163.com>
2014-06-09 21:03:36 +0800

    Create macros.h

---
+++ algos/macros.h
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+#define REP(i, n) for(int _n = n, i = 0; i < _n; i++)
+#define FOR(i, a, b) for(int i = (a), _b = (b); i <= _b; i++)
+#define Max(a, b) ((a) > (b) ? (a) : (b))
+#define Min(a, b) ((a) < (b) ? (a) : (b))
+#define L(fmt, ...) do {if(true) printf(fmt"\n", ##__VA_ARGS__);} while(false)
