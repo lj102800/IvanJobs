4a48d83b0593f042068d1e9bdf1d2d015891aea3
Ivan Jobs <ivan1377@163.com>
2014-07-19 11:25:37 +0800

    start CPPLib4Algo Lib with OO style

---
+++ CPPLib4Algorithms/Point2D.cpp
@@ -0,0 +1,13 @@
+class Point2D {
+	private:
+		double x;
+		double y;
+	public:
+		Point2D(double _x, double _y) : x(_x), y(_y) {}
+		static int ccw(Point2D& a, Point2D& b, Point2D& c) {
+			double area2 = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+			if (area2 < 0) return -1; // clockwise
+			else if (area2 > 0) return 1; // counter-clockwise
+			else return 0; // colinear
+		}
+};
