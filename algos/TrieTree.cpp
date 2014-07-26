90d07fda3f6c0e9715d1df36e27fa24fd31a36a9
Ivan Jobs <ivan1377@163.com>
2014-07-26 19:46:15 +0800

    use Trie struct from white book

--- algos/TrieTree.cpp
+++ algos/TrieTree.cpp
@@ -1,66 +1,26 @@
-#include <cstdio>
 #include <cstring>
 
-struct node {
-	node(){
-		cnt = 0;
-		int i;
-		for (i = 0; i < 26; ++i)
-			next[i] = NULL;	
+#define MAXNODE 1000
+#define SIGMA 30
+
+struct Trie {
+	int ch[MAXNODE][SIGMA];
+	int val[MAXNODE];
+	int sz; // node size
+	Trie() { sz = 1; memset(ch[0], 0, sizeof(ch[0]));} // initially, only one root node
+	int idx(char c) { return c - 'a';} // index of c
+	
+	void insert(char* s, int v) {
+		int u = 0, n = strlen(s);
+		for (int i = 0; i < n; i++) {
+			int c = idx(s[i]);
+			if (!ch[u][c]) {
+				memset(ch[sz], 0, sizeof(ch[sz]));
+				val[sz] = 0;
+				ch[u][c] = sz++;
+			}
+			u = ch[u][c];
+		}
+		val[u] = v;
 	}
-	int cnt;
-	node* next[26];
 };
-
-
-#define MAX 1000
-node db[MAX];
-
-int curr = 0;
-node* root = &db[curr++];
-
-
-int main() {
-	int n;
-	char str[100];
-	while (scanf("%d", &n) && n != 0){
-		int i, j;
-		curr = 1;
-		for (i = 0; i < n; ++i){
-			scanf("%s", str);
-			// here we got str, and we need insert it into the trie tree.
-			node* p = root;
-			for (j = 0; j < strlen(str); ++j){
-				int ith = str[j] - 'a';
-				if (p->next[ith] != NULL){ //ok, we find it already exists, so add p->cnt++
-					p = p->next[ith];
-					p->cnt++;
-				} else {
-					p->next[ith] = &db[curr++];
-					p = p->next[ith];
-					p->cnt++;
-				}
-			}
-		}
-		scanf("%s", str); // this is prefix
-		//we will do searching in trie for prefix str
-		node* needle = root;
-		int res;
-		for (j = 0; j < strlen(str); ++j){
-			int slot = str[j] - 'a';
-			if (needle->next[slot] != NULL){
-				needle = needle->next[slot];
-				if ( j == strlen(str) - 1){
-					res = needle->cnt;
-					break;
-				}
-			} else {
-				res = -1;
-				break;
-			}
-		}
-
-		printf("res:%d\n", res);
-	}
-	return 0;
-}
