dcacd46ef6c630f10e656e37fe82d9e1c079a1b1
Ivan Jobs <ivan1377@163.com>
2014-09-23 15:53:39 +0800

    Update macros.h
    add RFOR, misusing of FOR macro as RFOR

--- algos/macros.h
+++ algos/macros.h
@@ -2,6 +2,7 @@
 
 #define REP(i, n) for(int _n = n, i = 0; i < _n; i++)
 #define FOR(i, a, b) for(int i = (a), _b = (b); i <= _b; i++)
+#define RFOR(i, b, a) for (int i = (b), _b = (a); i >= _b; i--)
 #define Max(a, b) ((a) > (b) ? (a) : (b))
 #define Min(a, b) ((a) < (b) ? (a) : (b))
 #define Abs(x) ((x) > 0 ? (x) :(-(x)))
